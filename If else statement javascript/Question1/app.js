049827d32c42571c5d46bec559989938c5fd6f19
Mustafa <153886730+MustafaRaheel26@users.noreply.github.com>
2024-03-04 10:34:59 +0500

    Add files via upload

---
+++ If else statement javascript/Question1/app.js
@@ -0,0 +1,10 @@
+
+var number = +prompt("Enter any Number"); 
+
+if (number % 3 === 0){
+    console.log("The Number is divisible by 3")
+
+} 
+else{
+   console.log("The Number is not divisible by 3")
+}
